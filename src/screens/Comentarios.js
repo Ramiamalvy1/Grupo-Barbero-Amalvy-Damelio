a1c83530afe8cc561a4ef757f6e624dd3b934fd4
Ramiamalvy1 <ramalvydegreef@udesa.edu.ar>
2026-06-15 21:37:31 -0300

    css comentarios

--- src/screens/Comentarios.js
+++ src/screens/Comentarios.js
@@ -101,20 +101,31 @@ function Comentarios(props) {
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f4f9fc', 
+        padding: 10,
+    },
+    loadingText: {
+        textAlign: 'center',
+        marginTop: 20,
+        color: '#74acdf',
+        fontWeight: 'bold',
+    },
     postCard: {
         backgroundColor: '#ffffff',
-        marginHorizontal: 15,
+        marginHorizontal: 12,
         marginVertical: 8,
         padding: 15,
-        borderRadius: 4,
+        borderRadius: 4,        
         borderLeftWidth: 4,
-        borderLeftColor: '#74acdf',
+        borderLeftColor: '#74acdf',   
         borderBottomWidth: 1,
         borderBottomColor: '#e0e0e0',
     },
     owner: {
         fontWeight: 'bold',
-        color: '#1e3d59',
+        color: '#1e3d59',         
         fontSize: 14,
         marginBottom: 6,
     },
@@ -135,8 +146,8 @@ const styles = StyleSheet.create({
         paddingTop: 8,
     },
     buttonLike: {
-        backgroundColor: '#74acdf',
-        padding: 8,
+        backgroundColor: '#74acdf',  
+        padding: 10,
         borderRadius: 4,
     },
     buttonTextLike: {
@@ -145,8 +156,8 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
     },
     buttonDislike: {
-        backgroundColor: '#e6f0fa',
-        padding: 8,
+        backgroundColor: '#e6f0fa',  
+        padding: 10,
         borderRadius: 4,
         borderWidth: 1,
         borderColor: '#74acdf',
@@ -156,10 +167,66 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         fontWeight: 'bold',
     },
-    buttonTextComment: {
+    sectionTitle: {
+        fontSize: 16,
+        fontWeight: 'bold',
         color: '#1e3d59',
+        marginHorizontal: 12,
+        marginTop: 15,
+        marginBottom: 8,
+    },
+    commentBox: {
+        backgroundColor: '#ffffff',
+        marginHorizontal: 12,
+        marginVertical: 4,
+        padding: 12,
+        borderRadius: 4,
+        borderBottomWidth: 1,
+        borderBottomColor: '#eee',
+    },
+    commentAuthor: {
+        fontWeight: 'bold',
+        color: '#1e3d59',
+        fontSize: 13,
+    },
+    commentContent: {
+        fontSize: 14,
+        color: '#444',
+        marginTop: 2,
+    },
+    noComments: {
+        textAlign: 'center',
+        margin: 20,
+        color: '#999',
+        fontSize: 14,
+    },
+    footer: {
+        paddingTop: 10,
+        borderTopWidth: 1,
+        borderTopColor: '#e0e0e0',
+        marginBottom: 10,
+    },
+    input: {
+        height: 42,
+        marginHorizontal: 12,
+        marginBottom: 8,
+        borderWidth: 1,
+        borderColor: '#74acdf', 
+        borderRadius: 4,
+        padding: 10,
+        backgroundColor: '#fff',
+    },
+    buttonPrimary: {
+        backgroundColor: '#74acdf',  
+        marginHorizontal: 12,
+        padding: 12,
+        borderRadius: 4,
+    },
+    buttonPrimaryText: {
+        color: '#ffffff',
         textAlign: 'center',
         fontWeight: 'bold',
     }
 });
+
 export default Comentarios;
